4f5bf292cae9a141ad9e92e2e061122b7cac2841
trickshot2012 <trickshot2012@yahoo.de>
2020-05-16 09:46:44 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var request = new HelloRequest
+            {
+                Name = "Mr. X"
+
+            };
+            var response=await client.SayHelloAsync(request);
+            var response2 = await client.SayHelloAsync(request);
+            Console.WriteLine(response.Message);
+            Console.WriteLine(response2.Message);
+            Console.WriteLine("Press <Enter> for 3. request ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            var response3 = await client.SayHelloAsync(request);
+            Console.WriteLine(response3.Message);
+            Console.WriteLine("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
